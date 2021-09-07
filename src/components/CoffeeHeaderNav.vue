35c072ed17f4ad4a164723f06fb9eda642cb29c3
Thinkedou <r.enguehard@gmail.com>
2021-09-07 15:34:09 +0200

    first!

---
+++ src/components/CoffeeHeaderNav.vue
@@ -0,0 +1,17 @@
+<template>
+   <h1 class="site-heading text-center text-white d-none d-lg-block">
+     <span class="site-heading-upper text-primary mb-3">Un Theme bootstrap pas du tout volé  (◔◡◔✿)</span>
+     <span class="site-heading-lower">Business Casual in Vue Component!</span>
+   </h1>
+</template>
+
+<script>
+
+
+export default {
+  name: 'CoffeeHeaderNav',
+  props: {
+
+  }
+}
+</script>
